6377b33fb570183c300e40d24e2c1b5f62621495
danielhe4rt <hey@danielheart.dev>
2021-08-13 17:06:20 -0300

    wip

--- resources/lang/pt_BR/views.php
+++ resources/lang/pt_BR/views.php
@@ -13,7 +13,7 @@ return [
             'streamers' => '<span class="infoPurple">:streamerCount Streamers</span> assinaram nosso manifesto.',
             'viewers' => '<span class="infoPurple">:viewerCount viewers</span> assinaram nosso manifesto.',
             'submitted' => 'Obrigado por apoiar! Clique para compartilhar',
-            'twitterUrl' => 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fsindicatodostreamer.com&text=Eu%20acabei%20de%20assinar%20o%20manifesto%20em%20prol%20dos%20streamers%20da%20@TwitchBR&hashtags=sindicatodostreamer',
+            'twitterUrl' => 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fsindicatodostreamer.com&text=Eu%20acabei%20de%20assinar%20o%20manifesto%20em%20prol%20dos%20streamers%20da%20@TwitchBR&hashtags=TwitchMelhore',
             'join' => 'Baixar Manifesto'
         ],
         'signatures' => [
